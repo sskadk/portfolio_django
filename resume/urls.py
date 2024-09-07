a4a85d977f6484f8bb72c4d7b6982499084491ff
Swastik Adhikari <78269644+sskadk@users.noreply.github.com>
2024-09-07 14:29:23 +0545

    update

--- resume/urls.py
+++ resume/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('projects', views.projects, name='projects'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('exp', views.exp, name='experience'),
+    # path('resume', views.resume, name='resume'),
+    path('certifications', views.certifications, name='certifications'),
 ]
--- resume/views.py
+++ resume/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html')
+
+def exp(request):
+    return render(request,'experience.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def projects(request):
+    return render(request,'project.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def certifications(request):
+    return render(request,'certification.html')
+
+# def resume(request):
+#     return render(request,'about.html')
